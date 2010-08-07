aa724fa8362d736d84d16bea68693f0385b111f8
derdon <liedtke.simon@googlemail.com>
2010-08-08 01:26:10 +0200

    removed log_functions.py

--- swsg/log_functions.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-def log_missing_package(pkg_name):
-    errmsg = 'The package "{0}" is not installed.'.format(pkg_name)
-    logging.error(errmsg)
-    # XXX: is exiting really necessary here?
-    sys.exit(1)
