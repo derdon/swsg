7635ece4d0424791eb13822fdb0532d3244ec56d
derdon <liedtke.simon@googlemail.com>
2010-08-16 00:52:13 +0200

    fixed ImportError and added LOGFILE to the SWSG package

--- swsg/__init__.py
+++ swsg/__init__.py
@@ -1,8 +1,11 @@
+from os import getenv, path
+
 from logbook import Logger
 from logbook.base import NOTICE
 
 __version__ = '0.1a'
+XDG_DATA_HOME = getenv(
+    'XDG_DATA_HOME', path.expanduser(path.join('~', '.local', 'share')))
+PROJECT_DATA_DIR = path.join(XDG_DATA_HOME, 'swsg')
 swsg_logger = Logger('SWSG Logger', level=NOTICE)
-XDG_DATA_HOME = os.getenv(
-    'XDG_DATA_HOME', os.path.expanduser(os.path.join('~', '.local', 'share')))
-PROJECT_DATA_DIR = os.path.join(XDG_DATA_HOME, 'swsg')
+LOGFILE = path.join(PROJECT_DATA_DIR, 'swsg.log')
