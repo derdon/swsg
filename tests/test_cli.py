22fbf83e69441aa7ea12e2b4b43ed18767d64ba6
derdon <liedtke.simon@googlemail.com>
2010-09-07 18:13:15 +0200

    added tests for changing configuration values via the CLI

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,8 +1,20 @@
-from py.io import StdCapture
+import py
 from swsg.cli import parse_args
 from swsg import __version__ as swsg_version
+from swsg.sources import SUPPORTED_MARKUP_LANGUAGES
+from swsg.templates import SUPPORTED_TEMPLATE_ENGINES
 
 args_without_any_options = parse_args(['list'])
+
+
+def pytest_generate_tests(metafunc):
+    funcargnames = set(metafunc.funcargnames)
+    supported_stuff = (SUPPORTED_MARKUP_LANGUAGES, SUPPORTED_TEMPLATE_ENGINES)
+    funcarg_is_template = 'template' in funcargnames
+    key = 'template' if funcarg_is_template else 'markup'
+    if set(('markup', 'template')) & funcargnames:
+        for x in supported_stuff[funcarg_is_template]:
+            metafunc.addcall(funcargs={key: x})
 
 
 def test_verbose():
@@ -21,20 +33,41 @@ def test_debug():
     assert args_with_debug_long.debug
 
 
-def test_version():
-    capture = StdCapture()
+def test_version(capsys):
     try:
         parse_args(['--version'])
     except SystemExit, e:
         # exit code is 0
         assert e.code == 0
-        out, err = capture.done()
+        out, err = capsys.readouterr()
         # there should be no output to STDOUT
-        stdout = out.read()
-        assert stdout == u''
+        assert out == u''
         # version string is printed to STDERR
-        stderr = err.read()
         expected_version_string = 'py.test {0}\n'.format(swsg_version)
-        assert stderr == expected_version_string
+        assert err == expected_version_string
     else:
         assert False
+
+
+def test_change_config():
+    args = parse_args(['change-config'])
+    assert args.markup_language is None
+    assert args.template_language is None
+    py.test.raises(
+        SystemExit,
+        "parse_args(['change-config', '-t', 'non_existing_template_engine'])")
+    py.test.raises(
+        SystemExit,
+        "parse_args(['change-config', '-m', 'non_existing_markup_language'])")
+
+
+def test_markup_config(markup):
+    args = parse_args(['change-config', '-m', markup])
+    assert args.markup_language == markup
+    assert args.template_language is None
+
+
+def test_template_config(template):
+    args = parse_args(['change-config', '-t', template])
+    assert args.template_language == template
+    assert args.markup_language is None
