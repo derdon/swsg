4323d200912375f87c810678dc7a3dfeb03534ba
Simon Liedtke <simon@simon-liedtkes-imac.local>
2010-09-20 18:21:21 +0200

    moved constant paths to directories and file names into a seperate module called "file_paths"

--- swsg/__init__.py
+++ swsg/__init__.py
@@ -1,11 +1,5 @@
-from os import getenv, path
-
 from logbook import Logger
 from logbook.base import NOTICE
 
 __version__ = '0.1a'
-XDG_DATA_HOME = getenv(
-    'XDG_DATA_HOME', path.expanduser(path.join('~', '.local', 'share')))
-PROJECT_DATA_DIR = path.join(XDG_DATA_HOME, 'swsg')
 swsg_logger = Logger('SWSG Logger', level=NOTICE)
-LOGFILE = path.join(PROJECT_DATA_DIR, 'swsg.log')
---
+++ swsg/file_paths.py
@@ -0,0 +1,6 @@
+from os import getenv, path
+
+XDG_DATA_HOME = getenv(
+    'XDG_DATA_HOME', path.expanduser(path.join('~', '.local', 'share')))
+PROJECT_DATA_DIR = path.join(XDG_DATA_HOME, 'swsg')
+LOGFILE = path.join(PROJECT_DATA_DIR, 'swsg.log')
