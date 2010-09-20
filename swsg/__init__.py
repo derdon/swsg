95bda1df93a1323d340b337a6e380e0abb2f513c
Simon Liedtke <simon@simon-liedtkes-imac.local>
2010-09-20 18:28:41 +0200

    moved definition of default logger into a seperate module so that the swsg/__init__.py does not depend on logbook anymore

--- swsg/__init__.py
+++ swsg/__init__.py
@@ -1,5 +1,1 @@
-from logbook import Logger
-from logbook.base import NOTICE
-
 __version__ = '0.1a'
-swsg_logger = Logger('SWSG Logger', level=NOTICE)
---
+++ swsg/loggers.py
@@ -0,0 +1,4 @@
+from logbook import Logger
+from logbook.base import NOTICE
+
+swsg_logger = Logger('SWSG Logger', level=NOTICE)
