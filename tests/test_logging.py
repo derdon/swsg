032cd560ce6e708921b2150d5178603627969d0d
derdon <liedtke.simon@googlemail.com>
2010-09-29 18:01:18 +0200

    added tests for the logging utilities in the cli module

---
+++ tests/test_logging.py
@@ -0,0 +1,39 @@
+import py
+from logbook import FileHandler, INFO, DEBUG
+from swsg.file_paths import LOGFILE as DEFAULT_LOGFILE
+from swsg.cli import get_logging_handler, set_logging_level
+
+
+class Object(object):
+    pass
+
+
+@py.test.mark.skipif('os.name != "nt"')
+def test_get_logging_handler_win():
+    args = Object()
+    args.logfile = None
+    py.test.raises(OSerror, 'get_logging_handler(args)')
+
+@py.test.mark.skipif('os.name == "nt"')
+def test_get_logging_handler():
+    args = Object()
+    args.logfile = None
+    handler = get_logging_handler(args)
+    assert isinstance(handler, FileHandler)
+    assert handler._filename == DEFAULT_LOGFILE
+
+
+def test_set_logging_level():
+    logger = Object()
+    logger.level_name = None
+    args = Object()
+    args.verbose = True
+    args.debug = False
+    new_logger = set_logging_level(args, logger)
+    assert new_logger.level_name == INFO
+    assert not new_logger.level_name == DEBUG
+    args.verbose = False
+    args.debug = True
+    new_logger = set_logging_level(args, logger)
+    assert not new_logger.level_name == INFO
+    assert new_logger.level_name == DEBUG
