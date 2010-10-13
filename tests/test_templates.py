97602bb339144cc8963ccba4ce3903ea04e98180
derdon <liedtke.simon@googlemail.com>
2010-10-13 22:35:54 +0200

    fixed the test of SimpleTemplate

--- tests/test_templates.py
+++ tests/test_templates.py
@@ -6,11 +6,9 @@ import py.test
 from swsg.templates import BaseTemplate, SimpleTemplate
 from swsg.sources import ReSTSource, MarkdownSource
 
-SOURCE_FILENAME = u'temp-source.rest'
 SOURCE_TEXT = u'some **important** text'
-TEMPLATE_TEXT = string.Template(u'''sources: $temp_source
-<html><body><h1>{title}</h1><p>{content}</p></body></html>''').safe_substitute(
-    temp_source=SOURCE_FILENAME)
+SIMPLE_TEMPLATE_TEXT = (u'''sources: temp-source.rest
+<html><body><h1>{title}</h1><p>{content}</p></body></html>''')
 
 FIRST_REAL_LINE = u'first real line of the template'
 TEMPLATE_TEXT_ONESOURCE = (
@@ -47,39 +45,36 @@ def test_base_template_get_sources(tmpdir):
     assert second_source == assumed_source
 
 
-@py.test.mark.xfail
 def test_simple_template(tmpdir):
-    template_filename = tempfile.mkstemp()[1]
-    html_text = TEMPLATE_TEXT.format(title='$title', content='$content')
-    # write the template content into the temporary template file
-    with open(template_filename, 'w') as fp:
-        fp.write(html_text)
-    # create the source directory manually
-    tmpdir.ensure('sources', dir=True)
-    source_dir = tmpdir.join('sources')
-    # check if the source directory was created
-    assert source_dir.check()
-    # create a source file and fill it with content
-    with open(str(tmpdir.join('sources', SOURCE_FILENAME)), 'w') as fp:
-        fp.write(SOURCE_TEXT)
-    template = SimpleTemplate(str(source_dir), template_filename)
-    assert template.source_names == [SOURCE_FILENAME]
+    template_text = SIMPLE_TEMPLATE_TEXT.format(
+        title='$title', content='$content')
+    template_file = tmpdir.ensure('template.html')
+    template_file.check(file=True)
+    template_file.write(template_text)
+
+    source_filename = 'temp-source.rest'
+    source_filename_path = tmpdir.ensure(source_filename)
+    source_filename_path.check(file=True)
+    source_filename_path.write(SOURCE_TEXT)
+
+    template = SimpleTemplate(template_text)
+    source_names = list(template.source_names)
+    assert source_names == [source_filename]
     expected_result = (
         u'<html>'
           u'<body>'
-            u'<h1>temp-source</h1>'
+            u'<h1>some **important** text ...</h1>'
             u'<p>'
               u'<p>some <strong>important</strong> text</p>\n'
             u'</p>'
           u'</body>'
         u'</html>')
-    rendered_templates = template.render()
+    rendered_templates = template.render(str(tmpdir))
+    print list(template.source_names)
     source, output = rendered_templates.next()
     # make sure that there was only one template rendered
     py.test.raises(StopIteration, 'rendered_templates.next()')
     assert output == expected_result
-    # remove the temporary template file
-    os.remove(template_filename)
 
 
 def test_mako_template():
